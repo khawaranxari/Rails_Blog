25ed3315f151bad6999b054ef3c505e72031f871
Anxari <m.khawar.aslam0@gmail.com>
2020-03-06 15:57:53 +0500

    Added pg gem and removed sqlite

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: email)	
 		if user  && user.authenticate(params[:session][:password])
 			session[:user_id] = user.id
-			byebug
+			# byebug
 
 			flash[:success] = "You have logged in"
 			redirect_to users_path(user)
