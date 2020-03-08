f0f511f8b6049d0310473a21d804298acda78006
Anxari <m.khawar.aslam0@gmail.com>
2020-03-09 00:01:57 +0500

    first commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require ("jquery")
-require ("bootstrap-sprockets")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
